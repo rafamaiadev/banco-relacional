5c1b047fd0eb536f8bd4f782dba8f6157256ffd4
rafamaiadev <119112502+rafamaiadev@users.noreply.github.com>
2023-09-07 10:02:46 -0300

    att

--- consultarComJoin.sql
+++ consultarComJoin.sql
@@ -4,3 +4,11 @@ WHERE e.id = c.estado_id
 SELECT e.nome, c.nome, regiao 
 FROM estados e
 INNER JOIN cidades c ON e.id = c.estado_id
+
+SELECT * FROM cidades c
+LEFT JOIN prefeitos p 
+ON c.id = p.cidade_id
+
+SELECT * FROM cidades c
+RIGHT JOIN prefeitos p 
+ON c.id = p.cidade_id
